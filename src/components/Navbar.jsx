5f2324ee367540af722f76e8b60fc2c31434e87a
eniola <opakunleeniolail2019@gmail.com>
2024-04-10 12:11:26 +0100

    style button and add collapse button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,11 +5,14 @@ function Navbar() {
   return (
     <>
     <nav className='navbar navbar-expand-lg w-100'>
-      <div className='container-fluid justify-content-start w-100'>
+      <div className='container-fluid w-100'>
         <a href="#" className='navbar-brand'>
           <img src={logo} alt="logo" />
         </a>
-        <div className='collapse navbar-collapse' id='navbarNav'>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02"      aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div className='collapse navbar-collapse' id= "navbarTogglerDemo02">
           <ul className='navbar-nav'>
             <li className='nav-item'>
               <a href="#" className='nav-link active'>Home</a>
@@ -31,8 +34,8 @@ function Navbar() {
             <input type="text" placeholder='search'/>
           </form>
           <form action="">
-            <button className='btn btn-sm btn-outline-success'>Log in</button>
-            <button className='btn btn-sm btn-outline-success'>Signup</button>
+            <button className='btn btn-sm '>Log in</button>
+            <button className='btn btn-sm '>Signup</button>
           </form>
           {/* <div className=' d-flex'>
             <a href="" className='nav-link'>Home</a>
